23db5bef93e7fb5614760b1476aab5d3c64b613e
21t0636 <21t0636@bztf.ch>
2023-05-28 09:46:46 +0200

    Kleine Anpassungen an Code

--- Code/server.js
+++ Code/server.js
@@ -1,57 +1,8 @@
-/*
 const express = require('express');
 const app = express();
 const port = 3000;
 const nano = require('nano')('http://admin:admin@localhost:5984');
-const db = nano.db.use('demo');
-
-app.get('/', async (req, res) => {
-  try {
-    const response = await db.list({ include_docs: true });
-    const docs = response.rows.map(row => row.doc);
-
-    const tableRows = docs.map(doc => {
-      return `<tr>
-                <td>${doc._id}</td>
-                <td>${doc.name}</td>
-              </tr>`
-    }).join('');
-
-    const table = `
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-          </tr>
-        </thead>
-        <tbody>
-          ${tableRows}
-        </tbody>
-      </table>
-    `;
-
-    res.send(table);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('An error occurred');
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
-
-
-*/
-
-
-const express = require('express');
-const app = express();
-const port = 3000;
-const nano = require('nano')('http://admin:admin@localhost:5984');
-const db = nano.db.use('demo');
+const db = nano.db.use('projektarbeit-rezepte');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -62,7 +13,7 @@ app.get('/', async (req, res) => {
 
     let response;
     if (search) {
-      response = await db.search('demo', 'demo', { q: search, include_docs: true });
+      response = await db.search('projektarbeit-rezepte', 'projektarbeit-rezepte', { q: search, include_docs: true });
     } else {
       response = await db.list({ include_docs: true });
     }
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # M165-NoSQL-Projektarbeit
- 
+
+## Ausführen
+
+- cd in Code-Ordner (cd C:\Users\raffa\Desktop\M165_NoSQL_Projektarbeit\M165-NoSQL-Projektarbeit\Code)
+- node .\server.js
+    - http://localhost:3000/
+    - http://admin:admin@localhost:5984
+    - http://localhost@localhost:5984/_utils/#database/projektarbeit-rezepte/_all_docs
